7d9792700e26f76e8a62b7f42bca5f2aa75baf01
Roman <barabinr@list.ru>
2021-08-30 12:05:26 +0300

    optimized queries

--- main.py
+++ main.py
@@ -1,63 +1,79 @@
-from telegram import Bot
-from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
 import logging
-import requests
 import os
 import random
+import re
 
+import requests
 from dotenv import load_dotenv
+from telegram import Bot
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
 load_dotenv()
 
 WEATHER_KEY = os.getenv('weather_api_key')
 TELEGRAMM_TOKEN = os.getenv('TELEGRAMM_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
 
 logging.basicConfig(
     level=logging.INFO,
     filename='bot.log',
-    format='%(asctime)s %(levelname)s:  %(message)s'
+    format='%(asctime)s %(levelname)s:  %(message)s',
 )
 
 bot = Bot(token=TELEGRAMM_TOKEN)
 updater = Updater(token=TELEGRAMM_TOKEN, use_context=True)
-chat_id = 878886959
+chat_id = CHAT_ID
 
 weather_list = ['погода', 'погоду']
 
 
-def searh_func(message):
-    asc = message.strip().replace(' ', '%20')
-    response = requests.get(f'https://searx.roughs.ru/search?q={asc}&format=json&categories=news').json()
-    num_new = random.randint(1, 5)
-    first_new = response['results'][num_new]
-    logging.info(response['results'][0])
-    logging.info('Ответ "запрос/поиск" сформирован')
-    return f'{first_new["title"]}\n{first_new["content"]}\n{first_new["img_src"]}\n{first_new["pretty_url"]}'
-
-
-def get_weather():
-    response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q=Москва&appid={WEATHER_KEY}&lang=ru').json()
-    weather = response['weather'][0]
-    temp = round((response['main']['temp']) - 273.15, 1)
-    logging.info('Ответ "погода" сформирован')
-    return f'{(weather["description"]).capitalize()}.\nТемпература: {temp}'
-    
-
-def say_hi(update, context):
+def searh_func(update, context):
     chat = update.effective_chat
     message = (update.message.text).lower()
-    if [x for x in weather_list if x in message.split()]:
-        text = get_weather()
-    else:
-        text = searh_func(message)
+    asc = message.strip().replace(' ', '%20')
+    response = requests.get(
+        f'https://searx.roughs.ru/search?q={asc}&format=json&categories=news'
+    ).json()
+    num_new = random.randint(1, 5)
+    first_new = response['results'][num_new]
+    logging.info('Ответ "запрос/поиск" сформирован')
+    text = (
+        f'{first_new["title"]}\n{first_new["content"]}\n'
+        f'{first_new["img_src"]}\n{first_new["pretty_url"]}'
+    )
     context.bot.send_message(chat_id=chat.id, text=text)
-    logging.info('Сообщение отправлено.')
+    logging.info('Сообщение "searh_func" отправлено.')
+
+
+def get_weather(update, context):
+    chat = update.effective_chat
+    response = requests.get(
+        f'http://api.openweathermap.org/data/2.5/'
+        f'weather?q=Москва&appid={WEATHER_KEY}&lang=ru'
+    ).json()
+    weather = response['weather'][0]
+    temp = round((response['main']['temp']) - 273.15, 1)
+    text = f'{(weather["description"]).capitalize()}.\nТемпература: {temp}'
+    logging.info('Ответ "погода" сформирован.')
+    context.bot.send_message(chat_id=chat.id, text=text)
+    logging.info('Сообщение "get_weather" отправлено.')
+
 
 def wake_up(update, context):
     chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text='Спасибо что включили меня! :)')
+    context.bot.send_message(
+        chat_id=chat.id, text='Спасибо что включили меня! :)'
+    )
+    logging.info('Сообщение "wake_up" отправлено.')
+
 
 updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
+updater.dispatcher.add_handler(
+    MessageHandler(
+        Filters.regex(re.compile(r'погод.', re.IGNORECASE)), get_weather
+    )
+)
+updater.dispatcher.add_handler(MessageHandler(Filters.text, searh_func))
+
 
 updater.start_polling()
